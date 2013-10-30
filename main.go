4444a76d7c0c29b063b2795d27402e27dc492e0c
Tommi Virtanen <tv@eagain.net>
2013-10-30 11:55:01 -0700

    Clean up temp directories
    Used to not trigger deferred cleanup because of calling syscall.Exec.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"launchpad.net/goyaml"
 	"log"
@@ -72,43 +73,9 @@ type Spec struct {
 	}
 }
 
-func doit(args []string) error {
-	spec_path := args[0]
-
-	spec_base := filepath.Base(spec_path)
-	if spec_base[0] == '.' {
-		return fmt.Errorf("refusing to run hidden spec file: %s", spec_path)
-	}
-
-	// open it here to guard against typos; we don't need to read
-	// until we know it's a cache miss
-	spec_file, err := os.Open(spec_path)
-	if err != nil {
-		return fmt.Errorf("cannot open spec file: %v", err)
-	}
-	defer spec_file.Close()
-
-	cache_dir, err := getCacheDir()
-	if err != nil {
-		return err
-	}
-	cache_bin_dir := filepath.Join(cache_dir, "bin")
-	arch := fmt.Sprintf("%s_%s", runtime.GOOS, runtime.GOARCH)
-	cache_bin_arch_dir := filepath.Join(cache_bin_dir, arch)
-
-	binary := filepath.Join(cache_bin_arch_dir, spec_base)
-
-	if *run && !*upgrade {
-		err = runBinary(binary, flag.Args(), os.Environ())
-		if err != nil && !os.IsNotExist(err) {
-			return fmt.Errorf("cannot exec %s: %v", binary, err)
-		}
-	}
-
-	// if we're still here, we don't have a cached binary, or we're
-	// upgrading
-
-	err = maybeMkdirs(0750, cache_dir, cache_bin_dir, cache_bin_arch_dir)
+func build(cache_dir, cache_bin_dir, cache_bin_arch_dir string,
+	spec_path string, spec_file io.Reader, binary string) error {
+	err := maybeMkdirs(0750, cache_dir, cache_bin_dir, cache_bin_arch_dir)
 	if err != nil {
 		return fmt.Errorf("cannot create cache directory: %v", err)
 	}
@@ -180,6 +147,50 @@ func doit(args []string) error {
 	err = os.Rename(tmp_bin, binary)
 	if err != nil {
 		return fmt.Errorf("could put new binary in place: %v", err)
+	}
+	return nil
+}
+
+func doit(args []string) error {
+	spec_path := args[0]
+
+	spec_base := filepath.Base(spec_path)
+	if spec_base[0] == '.' {
+		return fmt.Errorf("refusing to run hidden spec file: %s", spec_path)
+	}
+
+	// open it here to guard against typos; we don't need to read
+	// until we know it's a cache miss
+	spec_file, err := os.Open(spec_path)
+	if err != nil {
+		return fmt.Errorf("cannot open spec file: %v", err)
+	}
+	// we rely on Go's automatic use of O_CLOEXEC to close this in
+	// syscall.Exec, this defer is a nice gesture for error cases
+	defer spec_file.Close()
+
+	cache_dir, err := getCacheDir()
+	if err != nil {
+		return err
+	}
+	cache_bin_dir := filepath.Join(cache_dir, "bin")
+	arch := fmt.Sprintf("%s_%s", runtime.GOOS, runtime.GOARCH)
+	cache_bin_arch_dir := filepath.Join(cache_bin_dir, arch)
+
+	binary := filepath.Join(cache_bin_arch_dir, spec_base)
+
+	if *run && !*upgrade {
+		err = runBinary(binary, flag.Args(), os.Environ())
+		if err != nil && !os.IsNotExist(err) {
+			return fmt.Errorf("cannot exec %s: %v", binary, err)
+		}
+	}
+
+	// if we're still here, we don't have a cached binary, or we're
+	// upgrading
+	err = build(cache_dir, cache_bin_dir, cache_bin_arch_dir, spec_path, spec_file, binary)
+	if err != nil {
+		return err
 	}
 
 	if *run {
